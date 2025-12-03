cf785e8845ff79b2b5de82599b6f179da27bb13a
rnato-cruz4 <renato.cruz@usach.cl>
2025-12-02 23:00:36 -0300

    creacion nuevos rf's2

--- codigo_fuente_21602350_CruzSantander/main_21602350_cruzsantander.pl
+++ codigo_fuente_21602350_CruzSantander/main_21602350_cruzsantander.pl
@@ -62,6 +62,7 @@ agregarLibro(BibliotecaIn, Libro1, BibliotecaOut) :-
 
 
 
+
 registrarUsuario(BibliotecaIn, Usuario, BibliotecaOut):-
 
     tdaUsuarioGetID(Usuario,IDNuevo),
@@ -79,3 +80,34 @@ obtenerUsuario(BibliotecaIn,IDBusca,UsuarioEncontrado):-
     tdaBibliotecaGetUsuarios(BibliotecaIn,ListaUsuarios),
 
     tdaUsuarioBuscarUsuario(IDBusca,ListaUsuarios,UsuarioEncontrado).
+
+
+buscarLibro(BibliotecaIn, Criterio, Valor, LibroOut) :-
+    % 1. Obtener la lista de libros
+    tdaBibliotecaGetLibros(BibliotecaIn, ListaLibros),
+    
+    % 2. Llamar al buscador recursivo
+    buscarLibroRec(ListaLibros, Criterio, Valor, LibroOut).
+
+
+
+
+buscarLibroRec([Libro|_], "id", ValorID, Libro) :-
+    tdaLibroGetID(Libro, ID),
+    ValorID =:= ID, !. % ! Detiene la búsqueda al encontrar el primero
+
+
+buscarLibroRec([Libro|_], "titulo", ValorBusqueda, Libro) :-
+    tdaLibroGetTitulo(Libro, TituloLibro), % El título ya está en minúsculas en el TDA
+    string_lower(ValorBusqueda, ValorLower), % Convertimos la búsqueda a minúsculas
+    sub_string(TituloLibro, _, _, _, ValorLower), !. % Verificamos si Valor está DENTRO de Titulo
+
+
+buscarLibroRec([Libro|_], "autor", ValorBusqueda, Libro) :-
+    tdaLibroGetAutor(Libro, AutorLibro),   % El autor ya está en minúsculas en el TDA
+    string_lower(ValorBusqueda, ValorLower),
+    sub_string(AutorLibro, _, _, _, ValorLower), !.
+
+
+buscarLibroRec([_|Resto], Criterio, Valor, LibroEncontrado) :-
+    buscarLibroRec(Resto, Criterio, Valor, LibroEncontrado).
